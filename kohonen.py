a3eca503a063826ca1d377820c8f50c5719d52c5
Lioz Elmalem <44621326+LiozElmalem@users.noreply.github.com>
2020-06-10 22:03:50 +0300

    First commit

---
+++ kohonen.py
@@ -0,0 +1,130 @@
+import math
+import numpy as np
+from itertools import permutations 
+import random
+import matplotlib.pyplot as plt
+
+class Kohonen:
+    def __init__(self , alpha , No , start , end , radius , topology):
+        neurons_X = None
+        neurons_Y = None
+        if topology == 'line':
+            self.neurons = []
+            neurons_X = np.random.uniform(low=start, high=end, size=No)
+            neurons_Y = [0] * No
+            for i , j in zip(neurons_X , neurons_Y):
+                self.neurons.append([i,j])
+        elif topology == 'circle':
+            self.neurons = generate_circle((0,0) , No , 1)     
+        elif topology == '5_on_5':
+            self.neurons = generate_5_5(2)
+        self.alpha = alpha
+        self.radius = radius
+
+    def best_match(self,chosen):
+        winner = self.neurons[0]
+        for i in self.neurons:
+            if euclidean_dist(chosen , i) < euclidean_dist(winner , chosen):
+                winner = i
+        return winner
+
+    def update(self , neuron , chosen):
+        for i in range(len(neuron)):
+            neuron[i] += self.alpha * (chosen[i] - neuron[i])
+
+    def update_neighbors(self , chosen , bmu):
+        for neuron in self.neurons:
+            if euclidean_dist(chosen , neuron) <= self.radius and neuron != bmu:
+                self.update(neuron,chosen)
+
+    def train(self , data , iterations):
+        plt.scatter([row[0] for row in self.neurons] , [row[1] for row in self.neurons] , c='green' , label='Neurons')
+        for i in range(len(self.neurons)):
+            for j in range(len(self.neurons)):
+                if (i != j):
+                    plt.plot([self.neurons[i][0],self.neurons[j][0]],[self.neurons[i][1],self.neurons[j][1]],'-ok',c='green')
+        plt.scatter([row[0] for row in data] , [row[1] for row in data] , c='blue', label='Training data')
+        plt.legend(loc="upper left")
+        plt.title('Kohonen init situation')
+        plt.show()
+        init_alpha = self.alpha
+        init_radius = self.radius
+        i = 0
+        while i <= iterations:
+            rand_chosen = random.choice(data)
+            bmu = self.best_match(rand_chosen)
+            self.update(bmu , rand_chosen)
+            self.update_neighbors(rand_chosen , bmu)
+            self.alpha = init_alpha * np.exp(- i / iterations)
+            self.radius = init_radius * np.exp(- i / iterations)
+            if i % 100 == 0:
+                draw(self.neurons , data , rand_chosen , bmu , i)
+            i += 1
+
+def draw(neurons , data , rand_chosen , bmu , i):
+    plt.scatter([row[0] for row in neurons] , [row[1] for row in neurons] ,c='green' , label='Neurons')
+    for ij in range(len(neurons)):
+        for j in range(len(neurons)):
+            if(ij != j):
+                plt.plot([neurons[ij][0],neurons[j][0]],[neurons[ij][1],neurons[j][1]],'-ok',c='green')
+    plt.scatter([row[0] for row in data] , [row[1] for row in data] , c='blue', label='Training data')
+    plt.scatter(rand_chosen[0],rand_chosen[1],c='red' , label='Random data point')
+    plt.scatter(bmu[0],bmu[1],c='yellow',label='BMU')
+    plt.legend(loc="upper left")
+    plt.title('Kohonen simulation , Iteration No. ' + str(i))
+    plt.show()
+
+def euclidean_dist(chosen , neuron):
+    return np.sqrt((chosen[0] - neuron[0])**2 + (chosen[1] - neuron[1])**2)
+
+def generate_circle(center, num_points, radius):
+    arc = (2 * math.pi) / num_points # what is the angle between two of the points
+    points = []
+    for p in range(num_points):
+        px = (0 * math.cos(arc * p)) - (radius * math.sin(arc * p))
+        py = (radius * math.cos(arc * p)) + (0 * math.sin(arc * p))
+        px += center[0]
+        py += center[1]
+        points.append([px,py])
+    return points
+
+def generate_5_5(density):
+    points = []
+    points.append([-2,-2])
+    points.append([-2,-1])
+    points.append([-2,0])
+    points.append([-2,1])
+    points.append([-2,2])
+    points.append([-1,-2])
+    points.append([-1,-1])
+    points.append([-1,0])
+    points.append([-1,1])
+    points.append([-1,2])
+    points.append([0,0])
+    points.append([0,1])
+    points.append([0,2])
+    points.append([0,-1])
+    points.append([0,-2])
+    points.append([1,-2])
+    points.append([1,-1])
+    points.append([1,0])
+    points.append([1,1])
+    points.append([1,2])
+    points.append([2,-2])
+    points.append([2,-1])
+    points.append([2,0])
+    points.append([2,1])
+    points.append([2,2])
+    # Normalize 
+    for point in points:
+        point[0] /= density
+        point[1] /= density 
+    return points
+
+def main():
+    net = Kohonen(0.5 , 30 , -2 , 2 , 2 , topology='circle')
+    circle = generate_circle((0,0) , 30 , 2)
+    net.train(circle , 1000)
+
+if __name__ == '__main__':
+    main()
